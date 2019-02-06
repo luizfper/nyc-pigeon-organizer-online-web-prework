760d76a993c34cda2a5f7a6f36d8e598a4e8ca42
Luiz Pereira <luizfper@gmail.com>
2019-02-06 01:26:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,7 +25,7 @@ def nyc_pigeon_organizer(data)
     value.each do |result,names|
       puts attributes
       names.each do |name|
-        pigeon_hash[name]=>{value=>result}
+        pigeon_hash[name]=result
       end
     end
   end
